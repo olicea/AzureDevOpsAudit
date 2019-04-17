a60a975275f353dbc7a0b945566b6344a5deafbc
Octavio Licea León <o.licea@gmail.com>
2019-04-17 08:13:18 -0400

    Adding download action, perhaps this can be a publish to disk

---
+++ auditlog/Actions/Download/DownloadAction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Services.Audit.WebApi;
+
+namespace auditlog
+{
+    public class DownloadAction : ICommandAction
+    {
+        public string Name => "download";
+
+        private IAuditLogProvider AuditLog { get; }
+
+        public DownloadAction(IAuditLogProvider auditLog, string searchString)
+        {
+            PublishTargets = targets;
+            AuditLog = auditLog;
+
+            CreatePublishers();
+        }
+
+        public async Task PerformAsync()
+        {
+            Console.WriteLine($"Downloading the log from {AuditLog.Source}");
+            Stopwatch watch =  Stopwatch.StartNew();
+            AuditLogQueryResult result;
+            long eventCount = 0;
+            int batchSize = 1000;
+            string continuationToken = null;
+            do
+            {
+                // query the log and store to the local cache
+                result = await AuditLog.QueryLogAsync(batchSize: batchSize, continuationToken: continuationToken);
+                HandleEntries(result.DecoratedAuditLogEntries);
+                continuationToken = result.ContinuationToken;
+                // Count how many entries have been downloaded
+                eventCount += result.DecoratedAuditLogEntries.Count();
+
+            } while (result.HasMore);
+
+            Console.WriteLine($"Published {eventCount} events in {watch.Elapsed}");
+        }
+
+        private void HandleEntries(IEnumerable<DecoratedAuditLogEntry> entries)
+        {
+            
+        }
+    }
+}
---
+++ auditlog/Actions/Download/WatermarkProvider.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Services.Audit.WebApi;
+
+namespace auditlog
+{
+    public class AuditLogWatermark
+    {
+        public string Source {get; set;}
+        public string Watermark {get; set;}
+    }
+
+    public interface IWatermarkProvider
+    {
+        AuditLogWatermark GetWatermark(string source);
+        void SetWatermark(AuditLogWatermark watermark);
+    }
+
+    public class FileWatermark : IWatermarkProvider
+    {
+        public const string DefaultFileName = "auditLogWatermark.json";
+        public string FileName;
+        
+        public AuditLogWatermark GetWatermark(string source)
+        {
+            JsonSerializer serializer = JsonSerializer.CreateDefault();
+            List<AuditLogWatermark> watermarks;
+            if (!File.Exists(FileName))
+            {
+                // The file does not exist creating an empty one and exit
+                watermarks = new List<AuditLogWatermark>()
+                {
+                    new AuditLogWatermark()
+                    {
+                        Source = source,
+                        Watermark  = null
+                    }
+                };
+
+                using (StreamWriter fileStream = File.CreateText(FileName))
+                using (JsonWriter jsonWriter = new JsonTextWriter(fileStream))
+                {
+                    serializer.Serialize(jsonWriter, watermarks);
+                    //throw new InvalidTargetSettingsException(Resources.AuditLog_Watermark_file_created + FileName);
+                }
+
+                return watermarks[0];
+            }
+            else
+            {
+                // The file is exists, try to deserialize the content
+                using (StreamReader fileStream = new StreamReader(FileName))
+                {
+                    string json = await fileStream.ReadToEndAsync();
+                    watermarks = JsonConvert.DeserializeObject<List<AuditLogWatermark>>(json);
+                }
+
+                return watermarks.FirstOrDefault(w => string.Equals(source, w.Source, StringComparison.OrdinalIgnoreCase));
+            }
+        }
+
+        public void SetWatermark(AuditLogWatermark value)
+        {
+
+        }
+    }
+}
